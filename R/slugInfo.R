9af5e9a2a220d7274326f100c1c80155e005003c
Bowen Chen <bwchen0719@gmail.com>
2020-06-07 17:12:47 -0500

    numerical value conversion
    Add a feature to convert numbers originally in characters to numerical values automatically. Also updated the documentations.

--- R/slugInfo.R
+++ R/slugInfo.R
@@ -1,4 +1,6 @@
-#' A data set that records the slug information
+#' A data set lists all available report information as supplied by USDA-AMS
+#'
+#' The listed reports describes slug ID, slug number, report title, published date, market, market type, office, and section names. The description of the report period always refers to the previous business days and not calendar days. During holidays periods, the “Reported Date” will be more than one day behind the “Report Date”. The time periods are in Central Time
 #'
 #' @docType data
 #'
--- R/utils.R
+++ R/utils.R
@@ -5,10 +5,37 @@ reshape_func <- function(data) {
   data_out
 }
 
-mpr_request_single <- function(slug, report_time, message = T){
-  slug <- paste0(slug)
+check_numeric <- function(dat){
+  dat_numeric <- suppressWarnings(as.numeric(gsub(',', '', dat)))
+  if(sum(dat_numeric, na.rm = T) == 0){
+    FALSE
+  }else{
+    TRUE
+  }
+}
+
+convert_date <- function(data){
+  out <- data
+  if(grep('published_date', colnames(data))){
+    out[, 'published_date'] <- as.POSIXct(out[, 'published_date'], format = "%m/%d/%Y %H:%M:%S")
+  }
+  if('report_date' %in% colnames(out)){
+    out[, 'report_date'] <- as.Date(out[, 'report_date'], format = "%m/%d/%Y")
+   }
+  if('slug_id' %in% colnames(out)){
+    out[, 'slug_id'] <- as.numeric(out[, 'slug_id'])
+  }
+  out <- dplyr::mutate_if(out, check_numeric, function(i) as.numeric(gsub(',', '', i)))
+  return(out)
+}
+
+mpr_request_single <- function(slug, report_time, message){
   validIDs <- c(2451, 2453, 2455:2464, 2466:2472, 2474:2489, 2498:2524, 2656, 2659:2681, 2685:2696, 2701:2703, 2989, 2991,
                 2993, 3345:3359)
+  # Check slug id
+  if(!as.numeric(slug) %in% validIDs) stop('Invalid slug ID. Please check with the slugInfo data set. Use data("slugInfo").')
+
+  slug <- paste0(slug)
 
   if(slug %in% c('2989', '2991', '2993')){# These slug ids are for dairy prices (starting from weekly).
     request_url <- paste0('https://mpr.datamart.ams.usda.gov/services/v1.1/reports/', slug, '?q=week_ending_date=', report_time, '&allSections=true')
@@ -37,6 +64,8 @@ mpr_request_single <- function(slug, report_time, message = T){
     }
     names(data_out) <- data$reportSection
   }
+  # Clean the dates and convert to numerical values.
+  data_out <- lapply(data_out, convert_date)
 
   if(isTRUE(message)){
     if(sum(grepl('No Results Found', data$message)) >= 1){
@@ -48,3 +77,4 @@ mpr_request_single <- function(slug, report_time, message = T){
   }
   return(data_out)
 }
+
